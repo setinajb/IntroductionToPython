d2ee667f90966c066a45a68df33167dba9eef3c3
setinajb <setinajb@rose-hulman.edu>
2017-11-27 08:54:22 -0500

    Completed m3_turtles

--- src/m3_turtles.py
+++ src/m3_turtles.py
@@ -14,7 +14,7 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 """
 ########################################################################
 #
-# TODO: 1.
+# DONE: 1.
 #  (Yes, that means for YOU to DO things per these instructions:)
 #
 # On Line 13 above, replace  PUT_YOUR_OWN_NAME_HERE  with your OWN name.
@@ -29,7 +29,7 @@ import rosegraphics as rg
 
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Allow this file to use the rosegraphics.py file by marking the src
 #   directory as a "Sources Root".  Do that by right clicking on the src folder,
 #   then selector  Mark Directory As --> Sources Root
@@ -73,11 +73,13 @@ matt.speed = 10  # Faster
 matt.backward(50)
 matt.left(90)
 matt.forward(50)
-
+matt.right(30)
+matt.forward(200)
+matt.draw_circle(100)
 
 ########################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Add a few more line of your own code above to make one of the
 #   existing Turtles move some more and/or have different
 #   characteristics.
@@ -91,7 +93,7 @@ matt.forward(50)
 
 ########################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   The code above  CONSTRUCTS  two SimpleTurtle objects and gives those objects NAMES:
 #       dave    matt
 #
@@ -109,6 +111,13 @@ matt.forward(50)
 #   As always, test by running the module.
 #
 ########################################################################
+
+jackie = rg.SimpleTurtle('turtle')
+jackie.pen = rg.Pen('blue', 30)
+jackie.speed = 10  # Faster
+jackie.backward(50)
+jackie.left(10)
+jackie.draw_square(150)
 
 ########################################################################
 #
